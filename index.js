45d66d47ddddabfbc95927a127ea8f7d6cdabd1a
Awo Lowos <pres_net@hotmail.com>
2021-09-11 10:36:11 -0400

    module.export Phrase

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+module.exports = Phrase;
+
 // Adds `reverse` to all strings.
-String.prototype.reverse = function() {
+String.prototype.reverse = function () {
   return Array.from(this).reverse().join("");
-}
+};
 
 // Defines a Phrase object.
 function Phrase(content) {
@@ -10,10 +12,10 @@ function Phrase(content) {
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
     return this.content.toLowerCase();
-  }
+  };
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
-  }
+  };
 }
