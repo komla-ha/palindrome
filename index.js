fe15f66891c540bb671e6d0767ccfab24a484b04
Awo Lowos <pres_net@hotmail.com>
2021-09-11 12:12:21 -0400

    et voilà

--- index.js
+++ index.js
@@ -11,11 +11,18 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
   };
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
+  };
+
+  this.letters = function letters() {
+    //return this.content; // stub return value
+    return Array.from(this.content)
+      .filter((x) => x.match(/[a-z]/i))
+      .join("");
   };
 }
